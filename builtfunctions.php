6d14b65ec6266c99fe573a8440a8504e61853359
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2022-07-08 16:45:19 +0200

    Saving changes daily

--- builtfunctions.php
+++ builtfunctions.php
@@ -1,1 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
+//MATH
+echo pow(2,7);
+echo "<br>";
+echo rand(1, 30000);
+echo "<br>";
+echo sqrt(100);
+echo "<br>";
+echo ceil(4.6);
+echo "<br>";
+echo floor(4.6);
+echo "<br>";
+echo round(4.5);
+echo "<br>";
+
+
+//STRING
+$string = "Hello my friend";
+echo "<br>";
+echo strlen($string);
+echo "<br>";
+echo strtoupper($string);
+echo "<br>";
+echo strtolower($string);
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
